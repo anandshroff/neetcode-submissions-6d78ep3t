70b5c55f4e00a0357f5bcffd1b197333e558919d
anandshroff <anandshroff5@gmail.com>
2026-05-25 15:33:13 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        for(auto &e: prerequisites){
+            int u = e[0];
+            int v = e[1];
+            adj[v].push_back(u);
+            
+        }
+        
+        queue<int>q;
+        int cnt = 0;
+        vector<int>indegree(numCourses, 0);
+        for(int i=0; i<numCourses; i++){
+            for(auto it: adj[i]){
+                indegree[it]++;
+            }
+        }
+
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            cnt++;
+
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+        
+        if(cnt < numCourses)
+            return false;
+        else
+            return true;
+    }
+};
