708694a3085f76da26773eadc4929c36f137d2d1
anandshroff <anandshroff5@gmail.com>
2026-07-12 22:24:05 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int l = 0;
+        for(int r = 0; r<prices.size(); r++){
+            if(prices[r] > prices[l]){
+                profit = max(profit, prices[r] - prices[l]);
+            }
+            else{
+                l = r;
+            }
+        }
+        return profit;
+    }
+};
