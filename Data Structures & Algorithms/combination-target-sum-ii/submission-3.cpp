ed7777e4014905f580f8f67d55914ef8010f7df1
anandshroff <anandshroff5@gmail.com>
2026-07-17 13:13:01 +0530

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    void findSum(int idx, vector<int> &nums, vector<int>&curr, vector<vector<int>>&ans,
+                    int target){
+        if(target == 0){
+            ans.push_back(curr);
+            return;
+        }
+        if(target < 0) return;
+        
+        for(int i = idx; i<nums.size(); i++){
+            if(i>idx && nums[i] == nums[i-1]) continue;
+            if(nums[i] > target) break;
+            curr.push_back(nums[i]);
+            findSum(i+1, nums, curr, ans, target - nums[i]);
+            curr.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        vector<vector<int>>ans;
+        vector<int>curr;
+        sort(nums.begin(), nums.end());
+        findSum(0, nums, curr, ans, target );
+        return ans;
+    }
+};
